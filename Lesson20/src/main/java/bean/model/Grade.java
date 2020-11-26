d1dc1ba7c2fbdb2d0b54ec52a2ef04fad7550451
longho <hoduylong411@gmail.com>
2020-11-26 22:48:18 +0700

    Lesson20 - 26.11.2020

---
+++ Lesson20/src/main/java/bean/model/Grade.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean.model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author USER
+ */
+public class Grade {
+    private Integer id;
+    private String name;
+
+    public Grade() {
+    }
+
+    public Grade(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        
+        if (!(obj instanceof Grade)) {
+            return false;
+        }
+        
+        Grade that = (Grade) obj;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
